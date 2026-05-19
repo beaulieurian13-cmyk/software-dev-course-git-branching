ed681c68d87533925feae57056271b3d1d27201b
beaulieurian13-cmyk <beaulieurian13@gmail.com>
2026-05-19 00:32:01 -0500

    Update author comment with purpose

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // Rian Beaulieu - 5-19-2026
+// This application prints "Hello, World!" and the numbers 0 to 9. At the end it also prints "Done" and "Hello to my new branch!"
 console.log("Hello world!");
 
 for (let i = 0; i < 10; i++) {
