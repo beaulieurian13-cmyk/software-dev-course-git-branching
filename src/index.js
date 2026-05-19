95fe75427cd4002d67086862b35397c14b0b9bab
beaulieurian13-cmyk <beaulieurian13@gmail.com>
2026-05-19 00:55:28 -0500

    resolve merge confilct are between new branch and main

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 // Rian Beaulieu - 5-19-2026
 // This application prints "Hello, World!" and the numbers 0 to 9. At the end it also prints "Done" and "Hello to my new branch!"
+//I followed the instrutions from the practice sheet in canvas. 
+// So I created a conflict resolve under the branch name "new-branch" when I merged it with main branch I chose both codes.
+//I did not realize that there was a read me file that I should have been using till I went to copy the url for the repository. 
+// I did the beginning of the assignment over. so it matchs the instructions.
+// I will make sure to use the read me file in the future. Hopefully it still counts even though it was not the exact wording and instructions.  
 console.log("Hello world!");
 
 for (let i = 0; i < 10; i++) {
